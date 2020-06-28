2483d8310b70754f1fd4441b92a09a6302424243
jliellen <43282021+jliellen@users.noreply.github.com>
2020-06-28 17:45:54 -0400

    Delete settings.py

--- settings.py
+++
@@ -1,9 +0,0 @@
-TRACK_TERMS = ["trump", "biden", "joe biden", "donald trump", "republican", "democratic"]
-CONNECTION_STRING = "sqlite:///tweets.db"
-CSV_NAME = "tweets.csv"
-TABLE_NAME = "election"
-
-try:
-    from private import *
-except Exception:
-    pass
